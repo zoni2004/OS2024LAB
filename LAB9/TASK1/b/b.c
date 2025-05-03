b99b1c9f2c4de19e451fcd61bf54c0e565953f7e
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2025-05-03 16:14:46 +0500

    Update b.c

--- LAB9/TASK1/b/b.c
+++ LAB9/TASK1/b/b.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
 
+void sigint_handler(int signum){
+      fprintf(stdout, "SIGINT received but ignored. Signal number: %d\n", signum);
+}
+
+int main(){
+     if(signal(SIGINT, sigint_handler) == SIG_ERR){
+         printf("Error setting up signal handler for SIGINT\n");
+         return 1;
+     }
+    
+     printf("Press Ctrl+C (SIGINT). It will be caught but ignored (not terminating)....\n");
+
+     while(1){
+       sleep(1);
+    }
+
+    return 0;
+}
