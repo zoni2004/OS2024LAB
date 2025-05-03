05596db7e0fc8bca61aec6cee4897eb30513e3ae
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2025-05-03 17:16:37 +0500

    Update e.c

--- LAB9/TASK1A/e/e.c
+++ LAB9/TASK1A/e/e.c
@@ -1,1 +1,18 @@
+#define _XOPEN_SOURCE 700
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 
+int main(){
+      signal(SIGINT, SIG_IGN);
+      
+      printf("Process PID: %d\n", getpid());
+      printf("Ctrl+C disabled. Use 'kill %d' from another terminal to terminate.\n", getpid());
+
+      while(1){
+         sleep(1);
+      }
+
+      return 0;
+}
