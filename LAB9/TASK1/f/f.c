d96796680e064f215c4138fe5ad84a82cac3783d
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2025-05-03 16:36:17 +0500

    Update f.c

--- LAB9/TASK1/f/f.c
+++ LAB9/TASK1/f/f.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
 
+void sigint_handler(int signum) {
+          fprintf(stdout, "Caught SIGNINT signal (%d)\n", signum);
+          raise(SIGKILL);
+}
+
+int main(){
+      if(signal(SIGINT, sigint_handler) == SIG_ERR){
+              printf("Error setting up a signal handler for SIGINT\n");
+              return 1;
+        }
+
+        printf("Press Ctrl+C to send a SIGINT signal\n");
+
+        while(1){
+              sleep(1);
+         }
+
+        return 0;
+}
