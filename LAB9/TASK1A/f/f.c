294c70d9f51f732e037a05510649c6acad3c6669
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2025-05-03 17:22:18 +0500

    Update f.c

--- LAB9/TASK1A/f/f.c
+++ LAB9/TASK1A/f/f.c
@@ -1,1 +1,31 @@
+#define _XOPEN_SOURCE 700
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 
+void sigint_handler(int signum){
+      printf("\nCtrl+C (SIGINT) received.Force-killing the process..\n");
+      fflush(stdout);
+      raise(SIGKILL);
+}
+
+int main(){
+      struct sigaction sa;
+      sa.sa_handler = sigint_handler;
+      sigemptyset(&sa.sa_mask);
+      sa.sa_flags = 0;
+
+      if(sigaction(SIGINT, &sa, NULL) == -1){
+         perror("sigaction");
+         return EXIT_FAILURE;
+      }
+      
+      printf("Press Ctrl+C to kil this process.\n");
+      printf("Process PID: %d\n", getpid());
+      while(1){
+         sleep(1);
+      }
+
+      return EXIT_SUCCESS;
+}
