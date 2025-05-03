c735b23aeeeda3fbe63dc34aef0e8120ce772a73
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2025-05-03 16:23:29 +0500

    Update c.c

--- LAB9/TASK1/c/c.c
+++ LAB9/TASK1/c/c.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+void sigint_handler(int signum){
+     fprintf(stdout, "Caught SIGINT signal (%d). Terminating program.\n" , signum);
+     exit(0);
+}
+
+int main(){
+     if(signal(SIGINT, sigint_handler) == SIG_ERR){
+         printf("Error setting up signal handler for SIGINT\n");
+         return 1;
+      }
+
+      printf("Press Ctrl+C (SIGINT). Program will catch and then terminate...\n");
+
+      while(1){
+         sleep(1);
+      }
+
+      return 0;
+}
